cee820f2cdb023bc1fd84a1201347f6afdbbd5db
lin.xiong <lin.xiong@husor.com>
2017-08-30 10:22:25 +0800

    fixbug

--- clear_lsh_sample.py
+++ clear_lsh_sample.py
@@ -18,11 +18,11 @@ emoji_pattern = re.compile(
     u"(\ud83c[\udde0-\uddff])"  
     "+", flags=re.UNICODE)
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'Usage: need two arguments doc_1.data doc_1_clear.data'
-        exit(-1)
-    input_data,output_data = sys.argv[1:]
-#    input_data,output_data = ('/home/lin.xiong/lsh_data/lsh.data','/home/lin.xiong/lsh_data/lsh_clear.data')
+#    if len(sys.argv) < 2:
+#        print 'Usage: need two arguments doc_1.data doc_1_clear.data'
+#        exit(-1)
+#    input_data,output_data = sys.argv[1:]
+    input_data,output_data = ('/home/lin.xiong/lsh_data/lsh.data','/home/lin.xiong/lsh_data/lsh_clear.data')
     clear_text = []
     with open(input_data,'r') as f:
         for line in f.readlines():
