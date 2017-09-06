ba4b61af3656a5b6c60a124b32be23bf89f0746f
lin.xiong <lin.xiong@husor.com>
2017-09-06 19:03:48 +0800

    add file

---
+++ get_hashcode_rawtext_file.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import os
+
+post_id = []
+raw_text = []
+with open('../lsh_data/day_902_903.data') as comment_file:
+	for line in comment_file:
+		post_id.append(line.strip().split('$&&$')[0])
+		raw_text.append(line.strip().split('$&&$')[1])
+print 'post_id  and raw_text length : ',len(post_id),len(raw_text)
+df_a = pd.DataFrame('post_id':post_id,'raw_text':raw_text)
+
+
+hash_post_id = []
+hash_code = []
+with open('../lsh_data/day_902_903_hash_code_file') as hash_code_file:
+	for line in hash_code_file:
+		hash_post_id.append(line.strip().split('\t')[0])
+		hash_code.append(line.strip().split('\t')[1])
+print 'hash_post_id and hash_code length :' len(hash_post_id),len(hash_code)
+
+df_b = pd.DataFrame('post_id':hash_post_id,'hash_code':hash_code)
+
+df_merge = pd.merge(df_a,df_b,how = 'left',on = 'post_id')
+
+info = zip(raw_text,hash_code)
+with open('hash_code_raw_text','w') as hash_code_raw_text:
+	res = []
+	for elem in info:
+		res.append(elem[1] + '\t' + elem[0] + os.linesep)
+	hash_code_raw_text.writelines(res)
